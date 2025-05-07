d561f71466349f1253bdfe9cae5cc2331be30416
Harry Liu <harrybaa@gmail.com>
2025-05-07 13:23:54 +0800

    feat: docker image

--- ui/src/utils/config.ts
+++ ui/src/utils/config.ts
@@ -1,4 +1,4 @@
 export const LOCAL_API_URL = 'http://localhost:8080'; 
-export const PROD_API_URL = 'https://demo-grocery.softprobe.ai';
+export const PROD_API_URL = import.meta.env.VITE_PROD_API_URL || 'http://114.215.188.138:8088';
 export const API_URL = import.meta.env.PROD ? PROD_API_URL : LOCAL_API_URL;
 export const LOGIN_TOKEN = 'login-token';
