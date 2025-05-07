e43e200dcff64581a17253fa62e594dc87c73b1f
Harry Liu <harrybaa@gmail.com>
2025-05-07 13:23:54 +0800

    Update prod API URL

--- ui/src/utils/config.ts
+++ ui/src/utils/config.ts
@@ -1,4 +1,4 @@
 export const LOCAL_API_URL = 'http://localhost:8080'; 
-export const PROD_API_URL = 'https://demo-grocery.softprobe.ai';
+export const PROD_API_URL = 'https://demo-springboot-mongodb.softprobe.ai';
 export const API_URL = import.meta.env.PROD ? PROD_API_URL : LOCAL_API_URL;
 export const LOGIN_TOKEN = 'login-token';
